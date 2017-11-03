991d041d28e38a7ee5b2e7822af21202216b779c
Jardine <jardine.miller@gmail.com>
2017-11-03 13:15:33 +0000

    Completed another challenge

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,49 +1,3 @@
-@pet_shop = {
-        pets: [
-          {
-            name: "Sir Percy",
-            pet_type: :cat,
-            breed: "British Shorthair",
-            price: 500
-          },
-          {
-            name: "King Bagdemagus",
-            pet_type: :cat,
-            breed: "British Shorthair",
-            price: 500
-          },
-          {
-            name: "Sir Lancelot",
-            pet_type: :dog,
-            breed: "Pomsky",
-            price: 1000,
-          },
-          {
-            name: "Arthur",
-            pet_type: :dog,
-            breed: "Husky",
-            price: 900,
-          },
-          {
-            name: "Tristan",
-            pet_type: :dog,
-            breed: "Basset Hound",
-            price: 800,
-          },
-          {
-            name: "Merlin",
-            pet_type: :cat,
-            breed: "Egyptian Mau",
-            price: 1500,
-          }
-        ],
-        admin: {
-          total_cash: 1000,
-          pets_sold: 0,
-        },
-        name: "Camelot of Pets"
-      }
-
 def pet_shop_name(hash)
 	return hash[:name]
 end
@@ -106,5 +60,9 @@ end
 
 def add_pet_to_customer(customer, pet)
 	customer[:pets] << pet
+end
+
+def customer_can_afford_pet(customer, pet)
+	return false if (customer[:cash] - pet[:price]) < 0 else return true
 end
 
--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+require 'minitest/rg'
 require_relative '../pet_shop'
 
 class TestPetShop < Minitest::Test
@@ -155,11 +156,11 @@ class TestPetShop < Minitest::Test
 
   # # OPTIONAL
 
-  # def test_customer_can_afford_pet__insufficient_funds
-  #   customer = @customers[1]
-  #   can_buy_pet = customer_can_afford_pet(customer, @new_pet)
-  #   assert_equal(false, can_buy_pet)
-  # end
+  def test_customer_can_afford_pet__insufficient_funds
+    customer = @customers[1]
+    can_buy_pet = customer_can_afford_pet(customer, @new_pet)
+    assert_equal(false, can_buy_pet)
+  end
 
   # def test_customer_can_afford_pet__sufficient_funds
   #   customer = @customers[0]
