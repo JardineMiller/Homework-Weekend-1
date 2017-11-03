668a0cd24e8bd64f1c355dc1ddc56bd1fb4c7532
Jardine <jardine.miller@gmail.com>
2017-11-03 15:21:33 +0000

    Completed all tasks

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,57 +1,57 @@
-def pet_shop_name(hash)
-	return hash[:name]
+def pet_shop_name(shop)
+	return shop[:name]
 end
 
-def total_cash(hash)
-	return hash[:admin][:total_cash]
+def total_cash(shop)
+	return shop[:admin][:total_cash]
 end
 
-def add_or_remove_cash(hash, amount)
-	current_cash = hash[:admin][:total_cash]
+def add_or_remove_cash(shop, amount)
+	current_cash = shop[:admin][:total_cash]
 	new_cash = current_cash + amount
-	hash[:admin][:total_cash] = new_cash
+	shop[:admin][:total_cash] = new_cash
 end
 
-def pets_sold(hash)
-	return hash[:admin][:pets_sold]
+def pets_sold(shop)
+	return shop[:admin][:pets_sold]
 end
 
-def increase_pets_sold(hash, amount)
-	current_pets_sold = hash[:admin][:pets_sold]
+def increase_pets_sold(shop, amount)
+	current_pets_sold = shop[:admin][:pets_sold]
 	new_pets_sold = current_pets_sold + amount
-	hash[:admin][:pets_sold] = new_pets_sold
+	shop[:admin][:pets_sold] = new_pets_sold
 end
 
-def stock_count(hash)
-	return hash[:pets].length
+def stock_count(shop)
+	return shop[:pets].length
 end
 
-def pets_by_breed(hash, breed)
+def pets_by_breed(shop, breed)
 	same_breed = []
-	pets = hash[:pets]
+	pets = shop[:pets]
 	for pet in pets
 		same_breed.push(pet) if pet[:breed] == breed
 	end
 	return same_breed
 end
 
-def find_pet_by_name(hash, name)
-	pets = hash[:pets]
+def find_pet_by_name(shop, name)
+	pets = shop[:pets]
 	for pet in pets
 		return pet if pet[:name] == name
 	end
 	return nil
 end
 
-def remove_pet_by_name(hash, name)
-	pets = hash[:pets]
+def remove_pet_by_name(shop, name)
+	pets = shop[:pets]
 	for pet in pets
 		pets.delete(pet) if pet[:name] == name
 	end
 end
 
-def add_pet_to_stock(hash, pet)
-	hash[:pets] << pet
+def add_pet_to_stock(shop, pet)
+	shop[:pets] << pet
 end
 
 def customer_pet_count(customer)
@@ -66,3 +66,17 @@ def customer_can_afford_pet(customer, pet)
 	return false if (customer[:cash] - pet[:price]) < 0 else return true
 end
 
+def sell_pet_to_customer(shop, pet, customer)
+  if pet == nil
+    return
+  elsif customer_can_afford_pet(customer, pet) == false
+    return
+  else
+    price = pet[:price]
+    before_count = customer_pet_count(customer)
+    customer[:pets] << pet
+    after_count = customer_pet_count(customer)
+    increase_pets_sold(shop, (after_count - before_count))
+    add_or_remove_cash(shop, price)
+  end
+end
